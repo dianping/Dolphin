6720fc45b3595550f2830ac65490b442d76032cb
chao.yu <chao.yu@dianping.com>
2015-12-11 11:20:19 +0800

    delete unused files

--- core/src/main/java/com/dianping/paas/core/service/AgentService.java
+++
@@ -1,8 +0,0 @@
-package com.dianping.paas.core.service;
-
-/**
- * chao.yu@dianping.com
- * Created by yuchao on 2015/12/02 14:20.
- */
-public interface AgentService {
-}
--- core/src/main/java/com/dianping/paas/core/service/ImageService.java
+++
@@ -1,8 +0,0 @@
-package com.dianping.paas.core.service;
-
-/**
- * Created by yuchao on 12/1/15.
- */
-public interface ImageService {
-    String getDockerfileTemplateContent(String image_type);
-}
